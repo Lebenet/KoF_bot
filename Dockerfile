b11c19fdad0e00427b7f02b6974503be6219ec2e
Lebenet <antoine.mulot2005@gmail.com>
2025-07-12 16:54:08 +0300

    fix dockerfile & fix import watcher.js

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,15 @@
-FROM node:latest
+FROM node:22-alpine
 
 # Create the bot directory
 RUN mkdir -p /usr/src/bot
 WORKDIR /usr/src/bot
 
 # Copy and install bot dependencies
-COPY package.json /usr/src/bot
+COPY package.json package-lock.json ./
 RUN npm install
 
-# Copy the bot itself
+# Copy the bot itself (Static Fallback)
 COPY . /usr/src/bot
 
 # Run the bot
 CMD ["./run.sh"]
-
--- utils/watcher.js
+++ utils/watcher.js
@@ -1,6 +1,6 @@
 const chokidar = require('chokidar');
 const fs = require('node:fs');
-// const path = require('node:path');
+const path = require('node:path');
 
 const { initLoad, unloadCommand, loadCommand, sendCommands, getCommands, getGuildCommands } = require('./commandLoader.js');
 const { loadConfig, addSingleConfig, /* updateSingleConfig, deleteSingleConfig, */ getConfig, lockBot, unlockBot, deleteSingleConfig } = require('./configLoader.js');
