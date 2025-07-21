0890cbd26b71614e3ef108e0a42150e1f3283584
Antoine <antoine.mulot2005@gmail.com>
2025-07-21 03:08:02 +0200

    fix(tsc): real final working fix to avoid duplicate TSC processes

--- run.sh
+++ run.sh
@@ -4,22 +4,22 @@ clear 2> /dev/null
 # Try to kill older tsc process
 mkdir -p processes
 if [[ -f processes/bot_tsc_watcher ]]; then
-	kill "$(cat processes/bot_tsc_watcher)" 2> /dev/null || true
+	pkill -f "tsc --watch"
 fi
+
+# Clean dist
+rm -rf ./dist/*
 
 # Run TypeScript compiler & watcher
 npx tsc --watch &
 
-# Save PID to stop the process when necessary
-echo $! > processes/bot_tsc_watcher
-
 # Send .env file & bot data over
-cp .env src/data dist/
+cp -r .env src/data dist/
 
-mkdir -p dist/commands/public dist/commands/dev dist/tasks/public dist/tasks/dev dist/data dist/temp
+mkdir -p dist/{commands,tasks}/{public,dev} dist/data dist/temp
 
 # Start docker
 docker compose up --build
 
 # If manually Ctrl+C'd the docker: kill tsc manually
-kill "$(cat processes/bot_tsc_watcher)" 2> /dev/null || true
+pkill -f "tsc --watch"
