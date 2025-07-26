94ab8e67026addeae20d614848bd3322c9f15917
Antoine <antoine.mulot2005@gmail.com>
2025-07-26 20:31:50 +0200

    fix(run): missing dist/ and db/

--- run.sh
+++ run.sh
@@ -2,21 +2,18 @@
 clear 2> /dev/null
 
 # Try to kill older tsc process
-mkdir -p processes
-if [[ -f processes/bot_tsc_watcher ]]; then
-	pkill -f "tsc --watch"
-fi
+pkill -f "tsc --watch"
 
 # Clean dist
+mkdir -p dist db
 rm -rf ./dist/*
+mkdir -p dist/{commands,tasks}/{public,dev} dist/data dist/temp
 
 # Run TypeScript compiler & watcher
 npx tsc --watch &
 
 # Send .env file & bot data over
 cp -r .env src/data dist/
-
-mkdir -p dist/{commands,tasks}/{public,dev} dist/data dist/temp
 
 # Start docker
 docker compose up --build
