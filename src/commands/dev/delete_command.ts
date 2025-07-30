aee7f70f06acdbe03c78bb7c7b5a8892d1a29db4
Antoine <antoine.mulot2005@gmail.com>
2025-07-31 00:55:20 +0200

    feat(command): command to delete a command by command id, plus some utils & utils fixes

---
+++ src/commands/dev/delete_command.ts
@@ -0,0 +1,52 @@
+import {
+    ChatInputCommandInteraction,
+    MessageFlags,
+    PermissionFlagsBits,
+    SlashCommandBuilder,
+} from "discord.js";
+import { Config, Command } from "../../db/dbTypes";
+import { warningEmbed } from "../../utils/discordUtils";
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("delete_command")
+        .setDescription("Delete a command from the DB by its id")
+        .addStringOption((option) =>
+            option
+                .setName("id")
+                .setDescription("id de la commande")
+                .setRequired(true),
+        )
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+
+    execute: async (
+        interaction: ChatInputCommandInteraction,
+        config: Config,
+    ) => {
+        if (!config.admins?.includes(interaction.user.id)) {
+            interaction.reply({
+                content: "Not a bot admin",
+                flags: MessageFlags.Ephemeral,
+            });
+            return;
+        }
+
+        await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+
+        const command = new Command();
+        command.id = interaction.options.getString("id")!;
+        if (!command.delete()) {
+            interaction.editReply("Failed");
+            return;
+        }
+
+        interaction.editReply("Success");
+    },
+
+    help: warningEmbed({
+        title: "delete_command | Aide",
+        description:
+            "Une commande (du bot) pour supprimer une commande (de matériaux).\n\
+		S'utilise avec l'ID de la commande (de matériaux).",
+    }),
+};
--- src/utils/taskUtils.ts
+++ src/utils/taskUtils.ts
@@ -15,6 +15,28 @@ export function fakeParisTimeToUTC() {
     return new Date(now.getTime() + parisOffset * 60 * 1000);
 }
 
+export function getParisDatetimeSQLiteSafe(date?: Date | null): string {
+    const now = date ? date : new Date();
+
+    const parts = new Intl.DateTimeFormat("en-GB", {
+        timeZone: "Europe/Paris",
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false,
+    }).formatToParts(now);
+
+    const map: Record<string, string> = {};
+    parts.forEach(({ type, value }) => {
+        if (type !== "literal") map[type] = value;
+    });
+
+    return `${map.year}-${map.month}-${map.day} ${map.hour}:${map.minute}:${map.second}`;
+}
+
 export function computeNextTimestamp(data: any) {
     // Task deactivated or no repeats left
     if (!data.activated || data.repeats === 0) return undefined;
